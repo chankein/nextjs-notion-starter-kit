67a0475e20affc7aa34613e48a5b848d45473ed7
chankeinjp@yahoo.co.jp <chankeinjp@yahoo.co.jp>
2022-07-04 12:39:00 +0900

    modify domain

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'bb076cca23f64a77a76886a5c34058e7',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Personal blog for kein',
+  domain: 'kein-blog.vercel.app',
+  author: 'ChanKein',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'notion-blog',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
